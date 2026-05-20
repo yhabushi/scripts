5443ac4c0a45cc7a6d42f721bb129bc5bb40ff4f
Yosi Habushi <32518341+yhabushi@users.noreply.github.com>
2026-05-20 14:26:43 +0300

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+
 echo "Starting process..."
+
+# 1. Potential Unary Operator Error (Missing quotes around variable)
 if [ $UNDEFINED_VAR == "hello" ]
 then
     echo "Found it"
+    
+    # 2. Syntax Error: Missing 'done' to close the loop
     for i in 1 2 3
     do
         echo $i
-    # missing 'done'
-fi
+    
+# 3. Syntax Error: Missing 'fi' (The shell will think 'fi' below is part of the string)
+# fi 
+
+# 4. Critical Syntax Error: Unclosed double quote
 echo "This line has an unclosed quote
+
+# 5. Logic Error: This code is unreachable because of the errors above
+echo "Process complete"
